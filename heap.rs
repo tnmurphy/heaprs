c3b6adc7260b1aec16a90d612a5343fde8e268c6
Timothy Norman Murphy <tnmurphy@gmail.com>
2025-08-14 10:01:18 +0300

    Can now add to the heap and maintain the condition that a parent is < its children. Now need to pop things off the heap

--- heap.rs
+++ heap.rs
@@ -6,6 +6,26 @@ fn right_child(item: usize)->usize {
     return item*2+1;
 }
 
+fn sortup(array: &mut Vec<i32>) {
+    let mut index = array.len()-1;
+
+    while index > 0 {
+        let parent = index >> 1;
+        println!("index = {} parent={}", index, parent);
+        if array[index] < array[parent] {
+        println!("swap {} {}", array[index], array[parent]);
+            array.swap(index,parent);
+        }
+        index = parent;
+    }
+}
+
+fn add(array: &mut Vec<i32>, item: i32) {
+
+    array.push(item);
+    sortup(array);
+}
+
 fn sort(array: &mut Vec<i32>) {
     let range = array.len();
 
@@ -28,15 +48,14 @@ fn sort(array: &mut Vec<i32>) {
 fn main() {
   let a = vec!(1i32, 4,3,2);
   let mut b:Vec<i32> = Vec::<i32>::with_capacity(20);
-
+  println!(" 3 >> 1 = {}", 3 >> 1);
   for item in &a {
      println!("{}",item);
   }
 
   for item in &a {
-      b.push(*item);
-      sort(&mut b);
-      println!("Inserted {}", item);
+      println!("Insertng {}", item);
+      add(&mut b, *item);
       for item2 in &b {
         println!("{}", item2);
       }
