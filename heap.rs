c4a6f053c10c9ab8cf1bc4e78e71bf8deea4028f
Timothy Norman Murphy <tnmurphy@gmail.com>
2025-08-16 16:45:37 +0300

    A heap as a struct.  This should eventually allow the implementation to change if needed - without affecting users.

--- heap.rs
+++ heap.rs
@@ -1,80 +1,87 @@
-// Little heap sort demo to learn 
-// rust. 
+// Little heap sort demo to learn
+// rust.
 // (c) T N Murphy.  This software is in the Public Domain
 
 
-fn sortup(array: &mut Vec<i32>) {
-    let mut index = array.len()-1;
-
-    while index > 0 {
-        let parent = index >> 1;
-        if array[index] < array[parent] {
-            array.swap(index,parent);
-        }
-        index = parent;
-    }
+struct Heap {
+    array: Vec<i32>,
 }
 
-fn add(array: &mut Vec<i32>, item: i32) {
 
-    array.push(item);
-    sortup(array);
-}
+impl Heap {
+    fn sortup(&mut self) {
+        let mut index = self.array.len() - 1;
 
-fn pop(array: &mut Vec<i32>) -> i32 {
-    let end = array.len();
-
-    //          0
-    //     1          2
-    //   3   4     5     6
-    //  7 8 9 10 11 12 13 14
-    //
-    
-
-    let item = array[0];
-    let mut parent = 0;
-    while parent < end {
-        let left_child = (parent << 1) + 1;
-        if left_child >= end {
-            break;
-        }
-
-        let right_child = left_child + 1;
-        if right_child >= end {
-            break;
-        }
-
-        if array[left_child] < array[right_child] {
-            array[parent] = array[left_child];
-            parent = left_child;
-        } else {
-            array[parent] = array[right_child];
-            parent = right_child;
+        while index > 0 {
+            let parent = index >> 1;
+            if self.array[index] < self.array[parent] {
+                self.array.swap(index, parent);
+            }
+            index = parent;
         }
     }
-    array.remove(parent);
-    return item;
+
+    fn add(&mut self, item: i32) {
+
+        self.array.push(item);
+        self.sortup();
+    }
+
+    fn pop(&mut self) -> i32 {
+        let end = self.array.len();
+
+        //          0
+        //     1          2
+        //   3   4     5     6
+        //  7 8 9 10 11 12 13 14
+        //
+
+
+        let item = self.array[0];
+        let mut parent = 0;
+        while parent < end {
+            let left_child = (parent << 1) + 1;
+            if left_child >= end {
+                break;
+            }
+
+            let right_child = left_child + 1;
+            if right_child >= end {
+                break;
+            }
+
+            if self.array[left_child] < self.array[right_child] {
+                self.array[parent] = self.array[left_child];
+                parent = left_child;
+            } else {
+                self.array[parent] = self.array[right_child];
+                parent = right_child;
+            }
+        }
+        self.array.remove(parent);
+        return item;
+    }
 }
 
 fn main() {
-  let a = vec!(1i32, 4,7,1,9,12,10,3,2,8,7);
-  let mut b:Vec<i32> = Vec::<i32>::with_capacity(20);
-  println!("The unsorted list:");
-  for item in &a {
-     println!("{}",item);
-  }
+    let h = &mut Heap { array: Vec::<i32>::with_capacity(20) };
 
-  for item in &a {
-      println!("Insertng {}", item);
-      add(&mut b, *item);
-      for item2 in &b {
-        println!("{}", item2);
-      }
-  }
-  println!("Now popping in order");
-  for _ in 0..b.len() {
-      let item = pop(&mut b);
-      println!("{}", item);
-  }
+    let a = vec![1i32, 4, 7, 1, 9, 12, 10, 3, 2, 8, 7];
+    println!("The unsorted list:");
+    for item in &a {
+        println!("{}", item);
+    }
+
+    for item in &a {
+        println!("Insertng {}", item);
+        h.add(*item);
+        for item2 in &h.array {
+            println!("{}", item2);
+        }
+    }
+    println!("Now popping in order");
+    for _ in 0..h.array.len() {
+        let item = h.pop();
+        println!("{}", item);
+    }
 }
-
