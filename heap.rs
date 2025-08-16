2678ca36fbc3e084d041d4e888ed8b4278658a6f
Timothy Norman Murphy <tnmurphy@gmail.com>
2025-08-16 16:15:03 +0300

    Remove an unused loop variable warning

--- heap.rs
+++ heap.rs
@@ -1,19 +1,14 @@
-fn left_child(item: usize)->usize {
-    return item*2;
-}
+// Little heap sort demo to learn 
+// rust. 
+// (c) T N Murphy.  This software is in the Public Domain
 
-fn right_child(item: usize)->usize {
-    return item*2+1;
-}
 
 fn sortup(array: &mut Vec<i32>) {
     let mut index = array.len()-1;
 
     while index > 0 {
         let parent = index >> 1;
-        println!("index = {} parent={}", index, parent);
         if array[index] < array[parent] {
-        println!("swap {} {}", array[index], array[parent]);
             array.swap(index,parent);
         }
         index = parent;
@@ -26,29 +21,45 @@ fn add(array: &mut Vec<i32>, item: i32) {
     sortup(array);
 }
 
-fn sort(array: &mut Vec<i32>) {
-    let range = array.len();
+fn pop(array: &mut Vec<i32>) -> i32 {
+    let end = array.len();
 
-    for item in 0usize..range {
-        let lc = left_child(item);
-        if lc < range {
-          if array[item] > array[lc] {
-              array.swap(item,lc);
-          }
+    //          0
+    //     1          2
+    //   3   4     5     6
+    //  7 8 9 10 11 12 13 14
+    //
+    
+
+    let item = array[0];
+    let mut parent = 0;
+    while parent < end {
+        let left_child = (parent << 1) + 1;
+        if left_child >= end {
+            break;
         }
-        let rc = right_child(item);
-        if rc < range {
-          if array[item] > array[rc] {
-            array.swap(item,rc);
-          }
+
+        let right_child = left_child + 1;
+        if right_child >= end {
+            break;
+        }
+
+        if array[left_child] < array[right_child] {
+            array[parent] = array[left_child];
+            parent = left_child;
+        } else {
+            array[parent] = array[right_child];
+            parent = right_child;
         }
     }
+    array.remove(parent);
+    return item;
 }
 
 fn main() {
-  let a = vec!(1i32, 4,3,2);
+  let a = vec!(1i32, 4,7,1,9,12,10,3,2,8,7);
   let mut b:Vec<i32> = Vec::<i32>::with_capacity(20);
-  println!(" 3 >> 1 = {}", 3 >> 1);
+  println!("The unsorted list:");
   for item in &a {
      println!("{}",item);
   }
@@ -59,6 +70,11 @@ fn main() {
       for item2 in &b {
         println!("{}", item2);
       }
+  }
+  println!("Now popping in order");
+  for _ in 0..b.len() {
+      let item = pop(&mut b);
+      println!("{}", item);
   }
 }
 
