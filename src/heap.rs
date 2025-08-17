c2ed4e7b5417d84de4df2dec99e584f61364a259
Timothy Murphy <tnmurphy@gmail.com>
2025-08-17 19:30:12 +0100

    Some more tests for the heap sort.

--- src/heap.rs
+++ src/heap.rs
@@ -64,33 +64,65 @@ impl Heap {
 }
 
 #[cfg(test)]
+
+
 mod test {
 
     use ::Heap;
 
-    #[test]
-    fn basic() {
-        let h = &mut Heap { array: Vec::<i32>::with_capacity(20) };
-        let numbers = vec![1i32, 4, 7, 1, 9, 12, 10, 3, 2, 8, 7];
-        let sorted_numbers = vec![1i32, 1, 2, 3, 4, 7, 7, 8, 9, 10, 12];
-        println!("The unsorted list:");
-        for item in &numbers {
-            println!("{}", item);
-        }
+    fn check_order(numbers: &[i32], expected: &[i32]) {
+        let h = &mut Heap { array: Vec::<i32>::with_capacity(numbers.len()+1) };
 
-        for item in &numbers {
-            println!("Insertng {}", item);
+        // Push the input onto the heap
+        for item in numbers {
             h.add(*item);
             for item2 in &h.array {
                 println!("{}", item2);
             }
         }
-        println!("Now popping in order");
+
+        // pop the numbers off the heap in order
+        println!("Popping:");
         for index in 0..h.array.len() {
             let item = h.pop();
-            let expected = sorted_numbers[index];
-            println!("{}-{}", item, expected);
+            let expected = expected[index];
+            println!("index {}:  got {}, expected {}", index, item, expected);
             assert_eq!(item, expected);
         }
+
+    }
+
+    #[test]
+    fn basic() {
+        let numbers = vec![1i32, 4, 7, 1, 9, 12, 10, 3, 2, 8, 7];
+        let sorted_numbers = vec![1i32, 1, 2, 3, 4, 7, 7, 8, 9, 10, 12];
+        check_order(&numbers, &sorted_numbers);
+    }
+    #[test]
+    fn one() {
+        let numbers = vec![1i32];
+        let sorted_numbers = vec![1i32];
+        check_order(&numbers, &sorted_numbers);
+    }
+
+    #[test]
+    fn reverse2() {
+        let numbers = vec![2i32, 1];
+        let sorted_numbers = vec![1i32, 2];
+        check_order(&numbers, &sorted_numbers);
+    }
+
+    #[test]
+    fn reverse3() {
+        let numbers = vec![3i32, 2, 1];
+        let sorted_numbers = vec![1i32, 2, 3];
+        check_order(&numbers, &sorted_numbers);
+    }
+
+    #[test]
+    fn reverse4() {
+        let numbers = vec![4i32, 3, 2, 1];
+        let sorted_numbers = vec![1i32, 2, 3, 4];
+        check_order(&numbers, &sorted_numbers);
     }
 }
